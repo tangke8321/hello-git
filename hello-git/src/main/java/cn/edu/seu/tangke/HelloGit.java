05fd4415cfa3d799b85153918f360a612bb2b49c
tangke <tangke8321@foxmail.com>
2020-09-29 10:09:14 +0800

    添加文件

---
+++ hello-git/src/main/java/cn/edu/seu/tangke/HelloGit.java
@@ -0,0 +1,11 @@
+package cn.edu.seu.tangke;
+
+/**
+ * @author tangke
+ * @date 2020/09/29
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
